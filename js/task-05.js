ef5f87608874f339254794abe4428f5dadac2759
dimon4uk1990 <mexal07@ukr.net>
2022-07-04 15:59:33 +0300

    Update task-5.js

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+input.addEventListener('input', onInputInput);
+
+
+function onInputChange(event) {
+    if (event.currentTarget.value === "") {
+        output.textContent = "Anonymous";
+    } else {
+        output.textContent = event.currentTarget.value;
+    }
+   
+};
+
+
