2139d51a6090092b44635b6244582b29480b5b29
dimon4uk1990 <mexal07@ukr.net>
2022-07-04 11:48:04 +0300

    1

--- js/task-01.js
+++ js/task-01.js
@@ -5,6 +5,4 @@ console.log(`Number of categories: ${categoriesElts.length}`);
 categoriesElts.forEach((element) => {
     return console.log(`Category: ${element.querySelector("h2").textContent}
 Elements:${element.querySelectorAll("li").length}`);
-})
-
-
+})  
