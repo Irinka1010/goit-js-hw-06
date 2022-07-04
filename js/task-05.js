4967ebf4bdfb5abd3fcf3ca9c58ab447700ff25a
dimon4uk1990 <mexal07@ukr.net>
2022-07-04 16:01:18 +0300

    заміна назви функції

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
-input.addEventListener('input', onInputInput);
+input.addEventListener('input', onInputChange);
 
 
 function onInputChange(event) {
