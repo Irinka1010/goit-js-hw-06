0de8f961ea547e799619a3a6f2970f14805ec540
dimon4uk1990 <mexal07@ukr.net>
2022-07-05 13:22:49 +0300

    1

--- js/task-09.js
+++ js/task-09.js
@@ -2,8 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
-
 const buttonEl = document.querySelector('.change-color');
 const spanEl = document.querySelector('.color');
 const body = document.querySelector('body');
