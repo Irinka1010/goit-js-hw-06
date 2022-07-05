d3541f9993ed427b88b5a278a711c72ee0bc2213
dimon4uk1990 <mexal07@ukr.net>
2022-07-05 13:15:53 +0300

    Update tatk-9.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const body = document.querySelector('body');
+
+
+buttonEl.addEventListener('click',onСhangeBackgroundColors);
+
+function onСhangeBackgroundColors(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = body.style.backgroundColor;
+}
