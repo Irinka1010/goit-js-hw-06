373e627b204e0d2d0f4619356b61c81b54dd7a7d
dimon4uk1990 <mexal07@ukr.net>
2022-07-07 15:49:41 +0300

    Привки в завдання 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,11 @@
 const inputEl = document.querySelector('#font-size-control');
 const spanText = document.querySelector('#text');
 inputEl.addEventListener('input', onChangesFontSize);
+spanText.style.fontSize= inputEl.value + 'px';
 
-function onChangesFontSize(event) {
+
+
+function onChangesFontSize() {
+   
     spanText.style.fontSize = `${inputEl.value}px`;
-}
+} 
